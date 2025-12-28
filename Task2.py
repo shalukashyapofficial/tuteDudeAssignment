89cf4179cf79a574305459615d269cead079f21f
shalukashyapofficial <shalukashyapofficial@gmail.com>
2025-12-28 22:00:49 +0530

    changes done

--- Task2.py
+++ Task2.py
@@ -1,4 +1,4 @@
 input1 = input("Enter your first name: ")
 input2 = input("Enter your second name: ")
 
-print("Hello!",input1,input2,"!  Welcome to Python!")
+print(f"Hello!,{input1} {input2}!  Welcome to Python!")
