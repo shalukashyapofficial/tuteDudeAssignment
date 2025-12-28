e0d32a8f07586d85a535fe643220354ebb547632
shalukashyapofficial <shalukashyapofficial@gmail.com>
2025-12-28 21:11:47 +0530

    Task 1 Completed

---
+++ Task1.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter first number :"))
+num2 = int(input("Enter second number:"))
+# Addition
+addition = num1 + num2
+print("Addition :",addition)
+
+subtraction = num1 - num2
+print("Substraction :",subtraction)
+
+multiplication = num1 * num2
+print("Multiplication :",multiplication)
+
+division = num1 / num2
+print("Division :",division)
---
+++ Task2.py
@@ -0,0 +1,4 @@
+input1 = input("Enter your first name: ")
+input2 = input("Enter your second name: ")
+
+print("Hello!",input1,input2,"!  Welcome to Python!")
